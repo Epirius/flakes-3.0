d988d85c2335fad491c09d516b843c68a2a75766
Felix Kaasa <mrKaasa@protonmail.com>
2024-06-12 23:25:48 +0200

    adding core home file

---
+++ home/fk/common/core/default.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, outputs, configLib, ...}:
+{
+  imports = (configLib.scanPaths ./.)
+    ++ (builtins.attrValues outputs.homeManagerModules);
+  
+  services.ssh-agent.enable = true;
+
+  home = {
+    username = lib.mkDefault "fk";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    stateVersion = lib.mkDefault "24.05";
+    sessionPath = [
+      "$HOME/.local.bin"
+    ];
+    sessionVariables = {
+      SHELL = "zsh";
+      TERM = "alacritty";
+      TERMINAL = "alacritty";
+      EDITOR = "nvim";
+    }
+  };
+
+  home.packages = builtins.attrValues {
+    inherit (pkgs)
+
+    # Packages that don't have costum configs go here
+    coreutils
+    eza
+    fd
+    findutils
+    fzf
+    ripgrep
+    tree
+    unzip
+    zip
+    wget
+  };
+
+  nixpkgs = {
+    overlays = builtins.attrValues outputs.overlays;
+    config = {
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
+
+  programs = {
+    home-manager.enable = true;
+  };
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+}
