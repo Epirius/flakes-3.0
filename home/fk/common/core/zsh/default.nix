2902ab81fc37e1fc5c147164faee6ad241dc79ac
Felix Kaasa <mrKaasa@protonmail.com>
2024-06-16 00:06:28 +0200

    added p10k

--- home/fk/common/core/zsh/default.nix
+++ home/fk/common/core/zsh/default.nix
@@ -5,6 +5,7 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    dotDir = ".config/zsh";
 
 
     shellAliases = {
@@ -27,6 +28,25 @@
     history = {
       size = 10000;
       # path = "${config.xdg.dataHome}/zsh/history";
+      share = true;
+    };
+
+    plugins = [
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k;
+        file = "p10k.zsh";
+      }
+      {
+        name = "zsh-powerlevel10k";
+        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+        file = "powerlevel10k.zsh-theme";
+      }
+    ];
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [ "git" "python" "rust" "sudo" ];
     };
 
     zplug = {
@@ -36,7 +56,17 @@
         { name = "MichaelAquilina/zsh-you-should-use"; }
         { name = "agkozak/zsh-z"; }
       ];
-      
     };
+
+    initExtraFirst = ''
+    # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+    # Initialization code that may require console input (password prompts, [y/n]
+    # confirmations, etc.) must go above this block; everything else may go below.
+    if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+      source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+    fi
+    '';
+
+
   };
 }
